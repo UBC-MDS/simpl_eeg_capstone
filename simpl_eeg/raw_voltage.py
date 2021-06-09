e35ab6aa0697f83f87892f91bec5090a279580ea
sbabicki <sbabicki@gmail.com>
2021-06-09 15:11:50 -0700

    Remove "Epoch Number" from bottom of raw voltage plot

--- simpl_eeg/raw_voltage.py
+++ simpl_eeg/raw_voltage.py
@@ -1,12 +1,13 @@
 import mne
-
-def plot_voltage(epoch, **kwargs):
+import matplotlib.pyplot as plt
+def plot_voltage(epoch, remove_xlabel=False, **kwargs):
     """
     Return interactive raw voltage plot
     
     Parameters
     ----------
     epoch (mne.epochs.Epochs): Epoch to display
+    remove_xlabel (bool, optional): Whether to remove the x axis label. Defaults to False.
     **kwargs : arguments
                raw, events=None, duration=10.0, start=0.0, n_channels=20, bgcolor='w', color=None, bad_color=(0.8, 0.8, 0.8), event_color='cyan', 
                scalings=None, remove_dc=True, order=None, show_options=False, title=None, show=True, block=False, highpass=None, lowpass=None, 
@@ -15,5 +16,10 @@ def plot_voltage(epoch, **kwargs):
     """
     if type(epoch) is not mne.epochs.Epochs:
         raise TypeError("epoch is not an epoched data, please refer to eeg_objects to create an epoched data")
-    
-    return epoch.plot(**kwargs)
+    fig = epoch.plot(**kwargs)
+
+    if remove_xlabel:
+        fig.axes[0].get_xaxis().set_visible(False)
+
+    return fig
+
