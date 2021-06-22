b81605fdd40cae6cba4564d0e71cf3f3efe7b876
sbabicki <sbabicki@gmail.com>
2021-06-21 18:11:45 -0700

    Generate report figures

--- Makefile
+++ Makefile
@@ -1,10 +1,21 @@
-all : reports/Proposal.pdf
+IMG_DEPS := reports/images/viz_example.png 
+IMG_DEPS += reports/images/raw_voltage.png 
+IMG_DEPS += reports/images/2d_head.png
+IMG_DEPS += reports/images/3d_head.png
+# IMG_DEPS += reports/images/3d_brain.png
+IMG_DEPS += reports/images/connectivity.png
+IMG_DEPS += reports/images/connectivity_circle.png
 
-reports/images/viz_example.png : reports/generate_figures.py data/927/.*
+all : reports/Final_Report.pdf
+
+$(IMG_DEPS) : reports/generate_figures.py data/927/.* 
 	python reports/generate_figures.py
 
 reports/Proposal.pdf : reports/Proposal.Rmd reports/images/TimelineGanttChart.pdf data/927/.*
-	Rscript -e "rmarkdown::render('reports/Proposal.Rmd',output_file='Proposal.pdf')"
+	Rscript -e "rmarkdown::render('reports/Proposal.Rmd', output_file='Proposal.pdf')"
+
+reports/Final_Report.pdf : reports/Final_Report.Rmd $(IMG_DEPS)
+	Rscript -e "rmarkdown::render('reports/Final_Report.Rmd', output_file='Final_Report.pdf')"
 
 ui :
 	streamlit run src/app.py
--- reports/generate_figures.py
+++ reports/generate_figures.py
@@ -1,9 +1,39 @@
-from mne.io import read_raw_eeglab
+import matplotlib
+matplotlib.use("Agg")
 
-example_file = "data/927/fixica.set"
-raw = read_raw_eeglab(example_file)
+from simpl_eeg import (
+    eeg_objects,
+    raw_voltage,
+    connectivity,
+    topomap_2d,
+    topomap_3d_brain,
+    topomap_3d_head
+)
+
+example_path = "data/927"
+epochs = eeg_objects.Epochs(example_path)
+epoch = epochs.epoch
+raw = epochs.eeg_file.raw
 
 table = raw.to_data_frame().head(10)
 
 fig = raw.plot(duration=2)
 fig.savefig("reports/images/viz_example.png")
+
+fig = raw_voltage.plot_voltage(epoch, remove_xlabel=True)
+fig.savefig("reports/images/raw_voltage.png")
+
+fig = topomap_2d.plot_topomap_2d(epoch)
+fig.figure.savefig("reports/images/2d_head.png")
+
+fig = topomap_3d_head.topo_3d_map(epoch, 0)
+fig.write_image("reports/images/3d_head.png")
+
+# fig = topomap_3d_brain.plot_topomap_3d_brain(epoch, backend='matplotlib')
+# fig.savefig("reports/images/3d_brain.png")
+
+fig = connectivity.plot_connectivity(epoch)
+fig.savefig("reports/images/connectivity.png")
+
+fig = connectivity.plot_conn_circle(epoch)
+fig.savefig("reports/images/connectivity_circle.png")
