0f89be2b37f7e2787cf374f1fc41bb00ba2370b9
Matthew Pin <MattTPin@gmail.com>
2021-06-24 19:24:35 -0700

    updated brain functionality and docs

--- tests/test_topomap_3d_brain.py
+++ tests/test_topomap_3d_brain.py
@@ -29,8 +29,8 @@ def test_create_inverse_solution():
 
     assert type(
     topomap_3d_brain.create_inverse_solution(
-        input_epoch,
-         input_fwd)) ==  mne.source_estimate.SourceEstimate
+        epoch=input_epoch,
+        fwd=input_fwd)) ==  mne.source_estimate.SourceEstimate
 
 
 with open('tests/test_data/test_stc.pickle', 'rb') as input:
@@ -40,8 +40,8 @@ with open('tests/test_data/test_stc.pickle', 'rb') as input:
 #     """Test cases for plotting 3D image mapped to the brain """
 #     expected_output = mne.viz._brain._brain.Brain
 
-#     assert  type(topomap_3d_brain.plot_topomap_3d_brain(epoch42[0],
-#                                   test_stc,
+#     assert  type(topomap_3d_brain.plot_topomap_3d_brain(
+#                                   stc = test_stc,
 #                                   views = "fro",
 #                                   backend = 'pyvista')) == expected_output
 
@@ -52,96 +52,84 @@ def test_plot_topomap_3d_brain_matplotlib():
 
     with pytest.raises(TypeError):
         topomap_3d_brain.plot_topomap_3d_brain(
-    "not an epoch", input_stc, backend='matplotlib')
+    epoch="not an epoch", backend='matplotlib')
 
     with pytest.raises(TypeError):
         topomap_3d_brain.plot_topomap_3d_brain(
-    input_epoch, "not an stc", 'matplotlib')
+    stc="not an stc", backend='matplotlib')
 
     with pytest.raises(TypeError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
-                                               display_time="not a display time",
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
+                                               recording_number="not a recording_number",
                                                backend='matplotlib')
 
     with pytest.raises(ValueError):
         topomap_3d_brain.plot_topomap_3d_brain(
-    input_epoch, input_stc, backend = "not a valid backend")
+    stc=input_stc, backend = "not a valid backend")
 
     with pytest.raises(TypeError):
         topomap_3d_brain.plot_topomap_3d_brain(
-    input_epoch, input_stc, views = 56, backend = 'matplotlib')
+    stc=input_stc, views = 56, backend = 'matplotlib')
 
     with pytest.raises(ValueError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                views = "not a valid view",
                                                backend = 'matplotlib')
 
     with pytest.raises(ValueError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                backend = "matplotlib",
                                                views = "cor")  # matplotlib doesn't take 'cor'
 
     with pytest.raises(ValueError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                views=["not a valid view",
                                                         "not a valid view 2"
                                                      ],
                                                backend = 'matplotlib')
 
     with pytest.raises(ValueError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                backend = 'matplotlib',
                                                views = ["lat", "cor"])
 
     with pytest.raises(ValueError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                backend = 'matplotlib',
                                                view_layout = 'not a valid layout')
 
     with pytest.raises(TypeError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                backend = 'matplotlib',
                                                size = 'not a valid size')
 
     with pytest.raises(ValueError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                backend = 'matplotlib',
                                                hemi = 'not a valid hemi')
 
     with pytest.raises(TypeError):
-        topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                               input_stc,
+        topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                backend = 'matplotlib',
                                                colorbar = 'not a valid hemi')
 
-    test_single_matplotlib=topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                                                    input_stc,
-                                                                    views = "fro",
-                                                                    hemi = 'lh',
-                                                                    backend = 'matplotlib')
+    test_single_matplotlib=topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
+                                                                  views = "fro",
+                                                                  hemi = 'lh',
+                                                                  backend = 'matplotlib')
 
     assert type(test_single_matplotlib) == matplotlib.figure.Figure
     assert len(test_single_matplotlib.axes) == 2
 
-    test_single_matplotlib_no_cb=topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                                                          input_stc,
-                                                                          views = "fro",
-                                                                          hemi = 'lh',
-                                                                          colorbar = False,
-                                                                          backend = 'matplotlib')
+    test_single_matplotlib_no_cb=topomap_3d_brain.plot_topomap_3d_brain(stc = input_stc,
+                                                                        views = "fro",
+                                                                        hemi = 'lh',
+                                                                        colorbar = False,
+                                                                        backend = 'matplotlib')
 
     assert len(test_single_matplotlib_no_cb.axes) == 1
     
-    test_multi_matplotlib = topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                                                   input_stc,
+    test_multi_matplotlib = topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                                    views = ['lat', 'fro'],
                                                                    hemi = 'both',
                                                                    backend = 'matplotlib',
@@ -150,8 +138,7 @@ def test_plot_topomap_3d_brain_matplotlib():
     assert  type(test_multi_matplotlib) == matplotlib.figure.Figure 
     assert  len(test_multi_matplotlib.axes) == 6
 
-    test_multi_matplotlib_no_cb = topomap_3d_brain.plot_topomap_3d_brain(input_epoch,
-                                                                         input_stc,
+    test_multi_matplotlib_no_cb = topomap_3d_brain.plot_topomap_3d_brain(stc=input_stc,
                                                                          views = ['lat', 'fro'],
                                                                          hemi = 'both',
                                                                          backend = 'matplotlib',
@@ -163,12 +150,10 @@ def test_plot_topomap_3d_brain_matplotlib():
 
 def test_animate_topomap_3d_brain_matplotlib():
     """Test cases for plotting 3D image mapped to the brain """
-    input_epoch = epoch42
     input_stc = test_stc
     
     with pytest.raises(TypeError):
         topomap_3d_brain.animate_matplot_brain(
-                input_epoch,
                 stc = test_stc,
                 hemi = 'lh',
                 views = ['lat'],
@@ -177,7 +162,6 @@ def test_animate_topomap_3d_brain_matplotlib():
     
     with pytest.raises(TypeError):
         topomap_3d_brain.animate_matplot_brain(
-                input_epoch,
                 stc = test_stc,
                 hemi = 'lh',
                 views = ['lat'],
@@ -185,7 +169,6 @@ def test_animate_topomap_3d_brain_matplotlib():
             );
     
     test_matplot_animate_single = topomap_3d_brain.animate_matplot_brain(
-        input_epoch,
         stc = test_stc,
         hemi = 'lh',
         views = ['lat']
@@ -194,7 +177,6 @@ def test_animate_topomap_3d_brain_matplotlib():
     assert  type(test_matplot_animate_single) == matplotlib.animation.FuncAnimation
     
     test_matplot_animate_multi = topomap_3d_brain.animate_matplot_brain(
-        input_epoch,
         stc = test_stc,
         hemi = 'both',
         views = ['lat', 'dor']
